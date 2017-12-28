0747c6a90505c086cd15a91e7711afdcf244891e
emorgan05 <emorgan05@gmail.com>
2017-12-28 16:06:03 -0500

    Custom address_attributes writer in market

--- app/models/market.rb
+++ app/models/market.rb
@@ -3,4 +3,9 @@ class Market < ApplicationRecord
   has_many :market_vendors
   has_many :vendors, through: :market_vendors
 
+  def address_attributes=(address_attributes)
+    if self.errors.messages.empty?
+      self.addresses.build(address_attributes)
+    end
+  end
 end
