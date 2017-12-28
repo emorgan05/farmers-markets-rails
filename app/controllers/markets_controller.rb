b88a8a0edb6d330e32dcb3a4397c23422e6c214a
emorgan05 <emorgan05@gmail.com>
2017-12-28 14:59:13 -0500

    Add markets controller and show action/view

---
+++ app/controllers/markets_controller.rb
@@ -0,0 +1,5 @@
+class MarketsController < ApplicationController
+  def show
+    @market = Market.find(params[:id])
+  end
+end
---
+++ test/controllers/markets_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MarketsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
