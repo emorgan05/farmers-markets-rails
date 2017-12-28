c557984cbdd86dbe01e097bd4a6e2043b589844c
emorgan05 <emorgan05@gmail.com>
2017-12-28 10:58:33 -0500

    Configuring Omniauth

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -4,5 +4,26 @@ class Vendor < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # omniauth
   devise :omniauthable, omniauth_providers: %i[facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |vendor|
+      vendor.email = auth.info.email
+      vendor.password = Devise.friendly_token[0,20]
+      # vendor.name = auth.info.name   # assuming the user model has a name
+      # vendor.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |vendor|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        vendor.email = data["email"] if vendor.email.blank?
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :vendors, controllers: { omniauth_callbacks: 'vendors/omniauth_callbacks' }
-
+  devise_scope :vendor do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_vendor_session
+  end
+  
   root 'welcome#home'
 end
