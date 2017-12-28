b021bd3688549dcf4d8fc79f85e00fde1ab7766b
emorgan05 <emorgan05@gmail.com>
2017-12-28 15:48:02 -0500

    Adding strong params to market controller

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -5,8 +5,14 @@ class MarketsController < ApplicationController
 
   def new
     @market = Market.new
+    @market.addresses.build
   end
 
   def create
+  end
+
+  private
+  def market_params
+    params.require(:market).permit(:name, :operating_hours, addresses_attributes: [:street_address_1, :street_address_2, :city, :state, :zip])
   end
 end
