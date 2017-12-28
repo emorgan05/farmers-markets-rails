b1118a997e36078bc96ccb8fa00878e3517898c2
emorgan05 <emorgan05@gmail.com>
2017-12-28 15:38:07 -0500

    Adding market#new and market#create actions

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -2,4 +2,11 @@ class MarketsController < ApplicationController
   def show
     @market = Market.find(params[:id])
   end
+
+  def new
+    @market = Market.new
+  end
+
+  def create
+  end
 end
--- app/models/market.rb
+++ app/models/market.rb
@@ -2,4 +2,5 @@ class Market < ApplicationRecord
   has_many :addresses
   has_many :market_vendors
   has_many :vendors, through: :market_vendors
+
 end
