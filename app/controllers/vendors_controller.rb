640729a5d867e18d22284195890e07318d48e22a
emorgan05 <emorgan05@gmail.com>
2017-12-29 16:00:47 -0500

    Adding gem pry, adding authorizations on vendor pages

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,5 +1,10 @@
 class VendorsController < ApplicationController
   def show
-    @vendor = Vendor.find(params[:id])
+    if current_vendor && current_vendor.id.to_s == params[:id]
+      @vendor = Vendor.find(params[:id])
+    else
+      flash[:notice] = "Please login to access your Vendor page"
+      redirect_to new_vendor_session_path
+    end
   end
 end
