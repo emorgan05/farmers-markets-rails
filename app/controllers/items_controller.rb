40b8cb37355df2a0e087787356b76156c69c9800
emorgan05 <emorgan05@gmail.com>
2017-12-29 12:26:02 -0500

    items#create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to vendor_items_path(params[:vendor_id])
+    else
+      render 'new'
+    end
   end
 
   private
