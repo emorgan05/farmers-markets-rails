2e07f5ad5234d0823571b8d273108cfa621e5f8f
emorgan05 <emorgan05@gmail.com>
2017-12-30 16:30:39 -0500

    Refactoring filter into a scope

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,5 @@ class Item < ApplicationRecord
   validates :price, numericality: { only_integer: true }
   validates :inventory, numericality: { only_integer: true }
 
-  def self.by_category(category_id)
-    where(category: category_id)
-  end
+  scope :by_category, ->(category_id) { where(category: category_id) }
 end
