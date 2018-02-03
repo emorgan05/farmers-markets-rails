d8c1a9b77996b2a0ab58cb6fe4beedaca9b380a5
emorgan05 <emorgan05@gmail.com>
2018-02-03 16:55:54 -0500

    adding get request for market#show info

--- app/assets/javascripts/markets.js
+++ app/assets/javascripts/markets.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
     event.preventDefault();
     var id = $(this).data("id");
     $.get("/markets/" + id, function(data) {
-      console.log(data);
+      $("#market_name").text(data.name);
+      $("#street_address_1").text(data.addresses[0].street_address_1);
+      $("#street_address_2").text(data.addresses[0].street_address_2);
+      var cityStateZip = data.addresses[0].city + ", " + data.addresses[0].state + " " + data.addresses[0].zip
+      $("#city_state_zip").text(cityStateZip);
+      $("#market_hours").text(data.operating_hours);
     });
   });
 });
