26f4c9ad28550fd89b829245d8a5736b37b1ce45
emorgan05 <emorgan05@gmail.com>
2018-02-05 10:42:25 -0500

    api request adds  markets#show to welcome#home. Debugged

--- app/assets/javascripts/markets.js
+++ app/assets/javascripts/markets.js
@@ -1,18 +1,20 @@
-$(document).ready(function() {
+$(function() {
   // markets#show
-  $("#js-marketShow").on("click", function(event) {
+  $(".js-marketShow").on("click", function(event) {
     event.preventDefault();
     var id = $(this).data("id");
-    $.get("/markets/" + id, function(data) {
-      $("#market_name").text(data.name);
-      $("#street_address_1").text(data.addresses[0].street_address_1);
-      $("#street_address_2").text(data.addresses[0].street_address_2);
-      var cityStateZip = data.addresses[0].city + ", " + data.addresses[0].state + " " + data.addresses[0].zip
-      $("#city_state_zip").text(cityStateZip);
-      $("#market_hours").text(data.operating_hours);
+    $.get("/markets/" + id + ".json", function(data) {
+      console.log(data);
+      $("#market_name_" + id).text(data["name"]);
+      $("#street_address_1_" + id).text(data["addresses"][0]["street_address_1"]);
+      $("#street_address_2_" + id).text(data["addresses"][0]["street_address_2"]);
+      var cityStateZip = data["addresses"][0]["city"] + ", " + data["addresses"][0]["state"] + " " + data["addresses"][0]["zip"]
+      $("#city_state_zip_" + id).text(cityStateZip);
+      $("#market_hours_" + id).text(data["operating_hours"]);
     });
   });
 });
+
 
 // function attachListeners() {
 //   $("#js-marketShow").on("click", function(event) {
