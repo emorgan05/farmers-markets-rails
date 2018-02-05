a9462b9307c1e8e0fee85a5275ec67afb40d4e25
emorgan05 <emorgan05@gmail.com>
2018-02-05 17:21:19 -0500

    Adding get request to items.js to get all items for a vendor

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+  // show item inventory
+  $(".js-inventory").on("click", function(event) {
+    event.preventDefault();
+    var vendor_id = $(this).data("vendor");
+    $.get("/vendor/" + vendor_id + "/items", function(data) {
+      console.log(data);
+    });
+  });
+});
