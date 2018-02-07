0cff52ccec9f05de2aa2856b434b062156278c35
emorgan05 <emorgan05@gmail.com>
2018-02-07 13:17:13 -0500

    Created ItemVendorSerializer to eliminate extra information when item is called

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :inventory
-  belongs_to :vendor
+  belongs_to :vendor, serializer: ItemVendorSerializer
   belongs_to :category
 end
---
+++ app/serializers/item_vendor_serializer.rb
@@ -0,0 +1,3 @@
+class ItemVendorSerializer < ActiveModel::Serializer
+  attributes :id, :shop_name
+end
