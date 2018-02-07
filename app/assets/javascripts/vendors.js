b46efc766018706f9ea193ac6402f5e6bb166a3c
emorgan05 <emorgan05@gmail.com>
2018-02-07 13:52:55 -0500

    Making vendor a Javascript Model Object with functions

--- app/assets/javascripts/vendors.js
+++ app/assets/javascripts/vendors.js
@@ -1,17 +1,18 @@
 // request for vendor information -- bound to document, because it is available at document.ready
+// on welcome#home page
 $(document).on("click", ".js-vendorDetails", function(event) {
   event.preventDefault();
   var id = $(this).data("id");
   var market_id = $(this).data("market");
   $.get("/markets/" + market_id + "/vendors/" + id + ".json", function(data) {
-    var vendorDetails = `<p>${data["shop_name"]}</p><p>${data["description"]}</p><p>${data["contact"]}</p>`;
-    $("#vendor_details_" + id).html(vendorDetails);
+    let vendor = new Vendor(data["id"], data["shop_name"], data["description"], data["contact"]);
+    vendor.renderDetails();
   });
 });
 
 // current vendor profile page
+// add edit form dynamically to the page
 $(document).ready(function() {
-  // add edit form dynamically to the page
   $(".js-vendorEdit").on("click", function(event) {
     event.preventDefault();
     var id = $(this).data("id");
@@ -29,11 +30,30 @@ $(document).on("submit", ".update_vendor", function(event) {
     url: this.action,
     data: $(this).serialize(),
     success: function(response) {
-      $("#js-shopName").text(response["shop_name"]);
-      $("#js-vendorName").text(response["shop_name"]);
-      $("#js-description").text(response["description"]);
-      $("#js-contact").text(response["contact"]);
-      $("#editVendor").html("");
+      let vendor = new Vendor(response["id"], response["shop_name"], response["description"], response["contact"]);
+      vendor.renderUpdates();
     }
   });
 });
+
+class Vendor {
+  constructor(id, shop_name, description, contact) {
+    this.id = id;
+    this.shop_name = shop_name;
+    this.description = description;
+    this.contact = contact;
+  }
+
+  renderDetails() {
+    var vendorDetails = `<p>${this.shop_name}</p><p>${this.description}</p><p>${this.contact}</p>`;
+    $("#vendor_details_" + this.id).html(vendorDetails);
+  }
+
+  renderUpdates() {
+    $("#js-shopName").text(this.shop_name);
+    $("#js-vendorName").text(this.shop_name);
+    $("#js-description").text(this.description);
+    $("#js-contact").text(this.contact);
+    $("#editVendor").html("");
+  }
+}
